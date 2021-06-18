f6dd4fd396b51e9cb9a9e3c46651c7acbe4dc5a7
Jaiaid <jaiaidmobin@gmail.com>
2021-06-19 02:57:47 +0600

    unnecessary statement remove, output format change

--- evac_route_generate.py
+++ evac_route_generate.py
@@ -7,7 +7,7 @@ import os
 
 
 
-gen_route_demand_tuple_list = []
+gen_route_demand_stoppoint_tuple_list = []
 pickup_point_list = []
 
 
@@ -61,11 +61,16 @@ def get_highest_demand_dst_to(dest, demand_matrix):
 
 def set_demand_satisfied_in_route(demand_matrix, route, dest):
     demand_matrix = demand_matrix.copy()
-    satisfied_demand = 0.
+    satisfied_demand = 0
+    stop_point_list = []
     for i in route:
         satisfied_demand += demand_matrix[i, dest]
-        demand_matrix[i][dest] = 0.
-    return demand_matrix, satisfied_demand
+        if demand_matrix[i][dest] != 0:
+            stop_point_list.append(i)
+        demand_matrix[i][dest] = 0
+    # finally include the shelter point (route last node/dest)
+    stop_point_list.append(dest)
+    return demand_matrix, satisfied_demand, stop_point_list
 
 
 def disconnect_nodes_in_route_from_graph(graph, route):
@@ -97,6 +102,8 @@ def get_route_satisfying_constraint(graph, demand_matrix, weight, min_hop_count,
     alter = False
     demand_matrix = demand_matrix.copy()
     source, dest = get_highest_demand_pair(demand_matrix)
+    # at least these two will be stops
+    stop_point_list = [source, dest]
     route = [source]
     iteration_count = 0
     while iteration_count < max_hop_count:
@@ -121,11 +128,13 @@ def get_route_satisfying_constraint(graph, demand_matrix, weight, min_hop_count,
             if len(route_to_dest) == 2 and graph[route_to_dest[0]][route_to_dest[1]]["weight"] == float("inf"):
                 break
             route.extend(route_chunk)
+            # add the newly added stop point
+            source
         else:
             alter = not alter
             break
 
-        demand_matrix, _ = set_demand_satisfied_in_route(demand_matrix, route, dest)
+        demand_matrix, _, _ = set_demand_satisfied_in_route(demand_matrix, route, dest)
         source = source2
         # source, dest = source2, get_highest_demand_destination_from(dest, demand_matrix)
         #if demand_matrix[source][dest] == 0.:
@@ -136,8 +145,9 @@ def get_route_satisfying_constraint(graph, demand_matrix, weight, min_hop_count,
     route_chunk = get_best_route_between(source, dest, graph, demand_matrix, weight)
 
     route.extend(route_chunk[1:])
-    demand_matrix, _ = set_demand_satisfied_in_route(demand_matrix, route, dest)
-    disconnect_nodes_in_route_from_graph(graph, route[:-1])
+    # followings are unnecessary as after this function will be exited and this copy of demand matrix will be lost
+    # demand_matrix, _, _ = set_demand_satisfied_in_route(demand_matrix, route, dest)
+    # disconnect_nodes_in_route_from_graph(graph, route[:-1])
     
     return route
 
@@ -146,17 +156,17 @@ def get_routes(graph, demand_matrix, weight, min_hop_count, max_hop_count):
     demand_matrix = demand_matrix.copy()
     while np.sum(demand_matrix) > 0.:
         route = get_route_satisfying_constraint(graph, demand_matrix, weight, min_hop_count, max_hop_count)
-        demand_matrix, satisfied_demand = set_demand_satisfied_in_route(demand_matrix, route, route[-1])
-        gen_route_demand_tuple_list.append((route, satisfied_demand))
+        # now to modify the demand matrix according to the serviced node's demand by this route
+        # node which has still non zero demand and will be made zero should be the stoppoint of route 
+        demand_matrix, satisfied_demand, stop_point_list = set_demand_satisfied_in_route(demand_matrix, route, route[-1])
+        gen_route_demand_stoppoint_tuple_list.append((route, satisfied_demand, stop_point_list))
         yield route
 
-def get_return_routes(graph, route_src_dest_pair_list):
-    for src_dest_pair in route_src_dest_pair_list:
-        src = src_dest_pair[1]
-        dest = src_dest_pair[0]
-        route = nx.algorithms.shortest_paths.weighted.dijkstra_path(graph, src, dest, weight=lambda u,v,d: d['weight'])
-        yield route
-
+def get_return_route(graph, src_dest_pair):
+    src = src_dest_pair[1]
+    dest = src_dest_pair[0]
+    route = nx.algorithms.shortest_paths.weighted.dijkstra_path(graph, src, dest, weight=lambda u,v,d: d['weight'])
+    return route
 
 def save_graph_as_json(distance_matrix, file_path):
     distance_matrix1 = distance_matrix.copy()
@@ -193,7 +203,7 @@ if __name__ == "__main__":
 
     #print('Average demand: {}'.format(demand_matrix[np.nonzero(demand_matrix)].mean()))
     #print('Total demand: {}'.format(demand_matrix.sum()))
-    print('{0}'.format(demand_matrix.sum()))
+    #print('{0}'.format(demand_matrix.sum()))
 
     with(open(sys.argv[3])) as pickup_point_file:
         for line in pickup_point_file.readlines():
@@ -205,7 +215,6 @@ if __name__ == "__main__":
 
     routes = list(get_routes(graph, demand_matrix, weight, min_hop_count, max_hop_count))
 
-    print(len(routes))
     for route in routes:
         try:
             assert(len(route) == len(set(route)), "node repeated in route")
@@ -213,19 +222,18 @@ if __name__ == "__main__":
             pass
 
     route_src_dest_pair_list_for_return_routes = []
-    for tup in sorted(gen_route_demand_tuple_list, key=lambda tup: tup[1], reverse=True):
-        print(tup[1])
-        route_src_dest_pair_list_for_return_routes.append((tup[0][0], tup[0][-1]))
-        for idx, node in enumerate(tup[0]):
+    for tup in sorted(gen_route_demand_stoppoint_tuple_list, key=lambda tup: tup[1], reverse=True):
+        return_route = get_return_route(graph, (tup[0][0], tup[0][-1]))
+        # at first list the stop point in route
+        for idx, node in enumerate(tup[2]):
             if idx > 0:
                print(' ', end='')
             print(node, end='')	
         print('')
-
-    return_routes = get_return_routes(graph, route_src_dest_pair_list_for_return_routes)
-    with open("return_route.txt", "w") as return_route_output_file:
-        for route in return_routes:
-            for node_id in route:
-                return_route_output_file.write(str(node_id) + ' ')
-            return_route_output_file.write(os.linesep)
+        # then write the route node + return route node
+        for idx, node in enumerate(tup[0]):
+            print(node, end=' ')
+        for node_id in return_route[1:]:
+            print(node_id, end=' ')
+        print('')
 
