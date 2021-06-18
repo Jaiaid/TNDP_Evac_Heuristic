3fa5b215a5d53dbe7981a25178394437b1c0ad93
Jaiaid <jaiaidmobin@gmail.com>
2021-06-19 02:59:49 +0600

    due to route generator code output format change return route file argument is removed

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ if  __name__ == "__main__":
     parser.add_argument("-df", "--demand_file", help="file path containing network demand information in 2d matrix", required=True)
     parser.add_argument("-pf", "--pickuppoint_file", help="file path containing pickuppoint id (corresponding to given network graph row position)", required=True)
     parser.add_argument("-rf", "--route_file", help="file path containing all forward route, satisfied demand by route information", required=True)
-    parser.add_argument("-rrf", "--return_route_file", help="file path containing return route, sequence is corresponding to route file", required=True)
     parser.add_argument("-o", "--output_dir", help="output directory path to dump generated data", required=True)
     
     args = vars(parser.parse_args())
@@ -26,7 +25,7 @@ if  __name__ == "__main__":
         network_file_path=args["network_file"], 
         demand_file_path=args["demand_file"], 
         pickuppoint_file_path=args["pickuppoint_file"], route_file_path=args["route_file"], 
-        return_route_file_path=args["return_route_file"], vehicle_dict=CONF.TRANSIT_VEHICLE_TYPE_PROP_DICT, 
+        vehicle_dict=CONF.TRANSIT_VEHICLE_TYPE_PROP_DICT, 
         start_time=CONF.TIME_TRANSIT_START, minute_between_start=CONF.TIME_BETWEEN_NEW_TRANSIT_RELEASE_MINUTE,
         max_roundtrip_minute=CONF.TIME_TRANSIT_RETURN_MINUTE, wait_minute_at_stop=CONF.TIME_WAITING_MIN
     )
--- matsim_input_generator.sh
+++ matsim_input_generator.sh
@@ -2,9 +2,8 @@
 
 OUT_DIR="halifax_matsim"
 IN_DIR="../data/Halifax"
-ROUTE_DIR="exp20210530_result"
+ROUTE_DIR="exp20210619_result"
 
 python3 main.py -nf $IN_DIR/HalifaxDistances.txt -df $IN_DIR/HalifaxOriginDestination.txt \
- -pf $IN_DIR/HalifaxPickupPoint.txt -rf $ROUTE_DIR/halifax_5_1_result.txt \
--rrf $ROUTE_DIR/halifax_5_1_return_route.txt -o $OUT_DIR
+ -pf $IN_DIR/HalifaxPickupPoint.txt -rf $ROUTE_DIR/halifax_5_1_result.txt -o $OUT_DIR
 
