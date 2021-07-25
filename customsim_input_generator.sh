15eef00d46a31b1d67c17b946c653fd5e736d177
Jaiaid <jaiaidmobin@gmail.com>
2021-07-25 14:12:38 +0600

    updated script for newly added cmd line argument

--- customsim_input_generator.sh
+++ customsim_input_generator.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-CUSTOMSIM_OUT_DIR="halifax_customsim"
 IN_DIR="../data/Halifax"
-ROUTE_DIR="exp20210619_result"
+ROUTE_DIR="exp20210717_result"
 
-python3 main.py -nf $IN_DIR/HalifaxDistances.txt -df $IN_DIR/HalifaxOriginDestination.txt \
- -pf $IN_DIR/HalifaxPickupPoint.txt -rf $ROUTE_DIR/halifax_5_1_result.txt -co $CUSTOMSIM_OUT_DIR
+MIN_HOP=5
+MAX_HOP=25
+WEIGHT=1
+SAME_RET_ROUTE=True
 
+CUSTOMSIM_OUT_DIR=halifax_${ROUTE_DIR}_${MIN_HOP}_${MAX_HOP}_customsim
+mkdir -p $CUSTOMSIM_OUT_DIR
+
+for hop_count in $(seq $MIN_HOP $MAX_HOP);
+do
+    python3 main.py -nf $IN_DIR/HalifaxDistances.txt -df $IN_DIR/HalifaxOriginDestination.txt \
+     -pf $IN_DIR/HalifaxPickupPoint.txt -rf $ROUTE_DIR/halifax_${hop_count}_1_result.txt  -smret $SAME_RET_ROUTE -co .
+    mv Bus_Stops.txt $CUSTOMSIM_OUT_DIR/Bus_Stops${hop_count}.txt
+    mv Bus_Edge.txt $CUSTOMSIM_OUT_DIR/Bus_Edge${hop_count}.txt
+    mv Bus_Fleet.txt $CUSTOMSIM_OUT_DIR/Bus_Fleet${hop_count}.txt
+    mv Bus_RouteStop.txt $CUSTOMSIM_OUT_DIR/Bus_RouteStop${hop_count}.txt
+done
